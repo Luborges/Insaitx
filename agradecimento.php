3745cc4484728fa10d1506c5c8b8f09c4e6961dd
MatosOliveira <matosalem.oliveira@gmail.com>
2017-05-23 18:45:34 -0300

    Inserção da página de agradecimento

---
+++ agradecimento.php
@@ -0,0 +1,34 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+	<meta charset="UTF-8">
+	<title>Agradecimento</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  	<meta name="robots" content="noindex nofollow">
+  	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+   	<style>
+  .form {
+    width: 50%;
+    float: none;
+    margin: 0 auto;
+	}
+  	</style>
+</head>
+<body>
+<?php include "topo.php"; ?>
+<section class="agradecimento">
+	<div class="container">
+		<div class="row">
+		  <div class="col-md-8 col-md-8 col-md-offset-2">
+		        <h1>Agradecimento</h1>
+			    <p>Obrigado por ser cadastrado em nosso site.</p>
+		  </div>
+		</div>
+	</div>
+</section>
+<?php include "footer.php"; ?>
+</body>
+</html>
